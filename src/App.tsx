be7a51e8c55e369e8c92a7a14442696212cff463
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2023-10-22 16:50:43 +0200

    Add responsive base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import "./App.css";
-import ListGroup from "./components/ListGroup";
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <ListGroup />
+      <Grid
+        templateAreas={{
+          base: `"nav main"`,
+          lg: `"nav nav""aside main"`,
+        }}
+        gridTemplateRows={"50px 1fr 30px"}
+        gridTemplateColumns={"150px 1fr"}
+        h="200px"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" bg="pink.300" area={"nav"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem pl="2" bg="blue.300" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem pl="2" bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
